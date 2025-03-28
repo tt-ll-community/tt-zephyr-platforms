68792ca14287af2e192f859ab7af56d74983809a
Alexander Lay <alexanderlay@tenstorrent.com>
2025-03-28 16:23:59 -0400

    bh_arc: Implement voltage/frequency margin
    Update VFCurve to use voltage and frequency margins from firmware table
    
    Signed-off-by: Alexander Lay <alexanderlay@tenstorrent.com>

--- lib/tenstorrent/bh_arc/vf_curve.c
+++ lib/tenstorrent/bh_arc/vf_curve.c
@@ -6,10 +6,23 @@
 
 #include <zephyr/sys/util.h>
 #include "vf_curve.h"
+#include "fw_table.h"
+
+/* Bounds checks for frequency and voltage margin */
+#define FREQ_MARGIN_MAX    300.0F
+#define FREQ_MARGIN_MIN    -300.0F
+#define VOLTAGE_MARGIN_MAX 150.0F
+#define VOLTAGE_MARGIN_MIN -150.0F
+
+static float freq_margin_mhz;
+static float voltage_margin_mv;
 
 void InitVFCurve(void)
 {
-	/* Do nothing for now */
+	freq_margin_mhz = CLAMP(get_fw_table()->chip_limits.frequency_margin, FREQ_MARGIN_MIN,
+				FREQ_MARGIN_MAX);
+	voltage_margin_mv = CLAMP(get_fw_table()->chip_limits.voltage_margin, VOLTAGE_MARGIN_MIN,
+				  VOLTAGE_MARGIN_MAX);
 }
 
 /**
@@ -20,7 +33,9 @@ void InitVFCurve(void)
  */
 float VFCurve(float freq_mhz)
 {
-	float voltage_mv = 0.00031395F * freq_mhz * freq_mhz - 0.43953F * freq_mhz + 828.83F;
+	float freq_with_margin_mhz = freq_mhz + freq_margin_mhz;
+	float voltage_mv = 0.00031395F * freq_with_margin_mhz * freq_with_margin_mhz -
+			   0.43953F * freq_with_margin_mhz + 828.83F;
 
-	return voltage_mv + 50.0F; /* Add 50 mV of margin */
+	return voltage_mv + voltage_margin_mv;
 }
