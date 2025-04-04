fa32db02c25f35f21fd9f1ff60ef6131dbf7487e
Daniel Rosen <drosen@tenstorrent.com>
2025-04-04 16:56:06 -0400

    Changed p300 bmc from a revision to a board type
    The p300 bmc is based on a completely different soc from the rest of the
    product line. To better reflect this difference I turned p300 into its
    own board. An ideal solution would involve moving the p300 into a
    seperate directory, but in the interest of going from 0 -> 1 I made the
    smallest diff I could.
    
    Signed-off-by: Daniel Rosen <drosen@tenstorrent.com>

--- boards/tenstorrent/tt_blackhole/revision.cmake
+++ boards/tenstorrent/tt_blackhole/revision.cmake
@@ -1,5 +1,5 @@
 set(BOARD_REVISIONS
-    "p100" "p100a" "p150" "p150a" "p150b" "p150c" "p300" "p300a" "p300b" "p300c")
-if(NOT DEFINED CONFIG_TT_SMC_RECOVERY AND NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
+    "p100" "p100a" "p150" "p150a" "p150b" "p150c" "p300a" "p300b" "p300c")
+if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
 message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for tt_blackhole. Accepted revisions: ${BOARD_REVISIONS}")
 endif()
